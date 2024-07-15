9f8225d7ad6bea16365e1c96ec11bcaa4fa16d66
Aarav Navani <38411399+oofmeister27@users.noreply.github.com>
2024-07-14 22:58:47 -0700

    code, readme and test

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -3,25 +3,39 @@
 
 from guardrails import Guard
 import pytest
-from validator import ValidatorTemplate
+from validator import cucumber_expression_match
+from cucumber_expressions.expression import CucumberExpression
+from cucumber_expressions.parameter_type import ParameterType
+from cucumber_expressions.parameter_type_registry import ParameterTypeRegistry
 
 # We use 'exception' as the validator's fail action,
 #  so we expect failures to always raise an Exception
 # Learn more about corrective actions here:
 #  https://www.guardrailsai.com/docs/concepts/output/#%EF%B8%8F-specifying-corrective-actions
-guard = Guard.from_string(validators=[ValidatorTemplate(arg_1="arg_1", arg_2="arg_2", on_fail="exception")])
+EXPRESSION = "I buy {positive_number} apple(s)/banana(s)/orange(s)"
 
-def test_pass():
-  test_output = "pass"
-  result = guard.parse(test_output)
-  
-  assert result.validation_passed is True
-  assert result.validated_output == test_output
+@pytest.mark.parametrize("good_text", [
+    "I buy 0 apples",
+    "I buy 1 apple",
+    "I buy 1 banana",
+    "I buy 2 bananas",
+    "I buy 1 orange",
+    "I buy 3 oranges",
+])
+def test_validate_on_success(self, good_text):
+  positive_number = ParameterType("positive_number", regexp=r"\d+", type=int)
+  validator = CucumberExpressionMatch(self.EXPRESSION, parameter_types=[positive_number], on_fail=OnFailAction.EXCEPTION)
+  result = validator.validate(good_text)
+  assert isinstance(result, PassResult) is True
 
-def test_fail():
-  with pytest.raises(Exception) as exc_info:
-    test_output = "fail"
-    guard.parse(test_output)
-  
-  # Assert the exception has your error_message
-  assert str(exc_info.value) == "Validation failed for field with errors: {A descriptive but concise error message about why validation failed}"
+@pytest.mark.parametrize("bad_text, reason", [
+        ("I buy 2 melons", "Unexpected fruit"),
+        ("I buy -10 apples", "Negative number"),
+    ])
+def test_validate_on_failure(self, bad_text, reason):
+  positive_number = ParameterType("positive_number", regexp=r"\d+", type=int)
+  validator = CucumberExpressionMatch(self.EXPRESSION, parameter_types=[positive_number], on_fail=OnFailAction.EXCEPTION)
+  result = validator.validate(bad_text)
+  assert isinstance(result, FailResult) is True
+  assert result.error_message == f"Result must match: {self.EXPRESSION}"
+  print(f"fix_value: {result.fix_value};")
--- validator/main.py
+++ validator/main.py
@@ -1,5 +1,7 @@
-from typing import Any, Callable, Dict, Optional
-
+from typing import Any, Callable, Dict, List, Optional
+from cucumber_expressions.expression import CucumberExpression
+from cucumber_expressions.parameter_type import ParameterType
+from cucumber_expressions.parameter_type_registry import ParameterTypeRegistry
 from guardrails.validator_base import (
     FailResult,
     PassResult,
@@ -7,42 +9,57 @@ from guardrails.validator_base import (
     Validator,
     register_validator,
 )
+import rstr
+import pytest
 
-
-@register_validator(name="guardrails/validator_template", data_type="string")
-class ValidatorTemplate(Validator):
-    """Validates that {fill in how you validator interacts with the passed value}.
+@register_validator(name="guardrails/cucumber_expression_match", data_type="string")
+class CucumberExpressionMatch(Validator):
+    """Validates that the input string matches a specified cucumber expression.
 
     **Key Properties**
 
-    | Property                      | Description                       |
-    | ----------------------------- | --------------------------------- |
-    | Name for `format` attribute   | `guardrails/validator_template`   |
-    | Supported data types          | `string`                          |
-    | Programmatic fix              | {If you support programmatic fixes, explain it here. Otherwise `None`} |
+    | Property                      | Description                                             |
+    | ----------------------------- | ------------------------------------------------------- |
+    | Name for `format` attribute   | `guardrails/cucumber_expression_match`                  |
+    | Supported data types          | `string`                                                |
+    | Programmatic fix              | Generate a string that matches this cucumber expression |
 
     Args:
-        arg_1 (string): {Description of the argument here}
-        arg_2 (string): {Description of the argument here}
+        expression (str): Cucumber expression to use (as string).
+        parameter_types (Optional[List[ParameterType]]): Parameter types to use (optional).
+        on_fail (str, Callable): The policy to enact when a validator fails. If `str`, must be one of `reask`, `fix`, `filter`, `refrain`, `noop`, `exception` or `fix_reask`. Otherwise, must be a function that is called when the validator fails.
     """  # noqa
 
-    # If you don't have any init args, you can omit the __init__ method.
     def __init__(
         self,
-        arg_1: str,
-        arg_2: str,
+        expression: str,
+        parameter_types: Optional[List[ParameterType]] = None,
         on_fail: Optional[Callable] = None,
     ):
-        super().__init__(on_fail=on_fail, arg_1=arg_1, arg_2=arg_2)
-        self._arg_1 = arg_1
-        self._arg_2 = arg_2
+        super().__init__(on_fail=on_fail)
+        parameter_types = parameter_types or []
+        self._expression = expression
+        self._parameter_type_registry = ParameterTypeRegistry()
+        for parameter_type in parameter_types:
+            self._parameter_type_registry.define_parameter_type(parameter_type)
 
     def validate(self, value: Any, metadata: Dict = {}) -> ValidationResult:
-        """Validates that {fill in how you validator interacts with the passed value}."""
-        # Add your custom validator logic here and return a PassResult or FailResult accordingly.
-        if value != "pass": # FIXME
+        """Validates that the input string matches the specified cucumber expression.
+
+        Args:
+            value (Any): The value to validate.
+            metadata (Dict): The metadata to validate against.
+
+        Returns:
+            ValidationResult: The validation result (PassResult or FailResult).
+        """
+        this_expression = CucumberExpression(self._expression, self._parameter_type_registry)
+        matched = this_expression.match(value)
+        if matched is None:
+            fix_string = rstr.xeger(this_expression.regexp)
             return FailResult(
-                error_message="{A descriptive but concise error message about why validation failed}",
-                fix_value="{The programmtic fix if applicable, otherwise remove this kwarg.}",
+                error_message=f"Result must match: {self._expression}",
+                fix_value=fix_string,
             )
+
         return PassResult()
